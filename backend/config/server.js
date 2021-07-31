5157f924767833b82f232be69f57b82b41bf3323
coffmanjrp <coffmanjrp@gmail.com>
2021-07-31 16:18:56 -0700

    Create strapi app

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2c65bb3757879d2ba6f669a168a75dde'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd193b843-6f24-4ed5-8bf7-a8a8914f1d77'
+};
