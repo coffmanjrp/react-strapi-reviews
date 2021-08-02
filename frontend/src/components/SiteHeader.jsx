af38269d1ac9f4b00eaf8ac91b6a796016ee544b
coffmanjrp <coffmanjrp@gmail.com>
2021-08-02 00:15:54 -0700

    Create a categoris relational data

--- frontend/src/components/SiteHeader.jsx
+++ frontend/src/components/SiteHeader.jsx
@@ -1,11 +1,34 @@
 import { Link } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
+
+const CATEGORIES = gql`
+  query GetCategories {
+    categories {
+      id
+      name
+    }
+  }
+`;
 
 const SiteHeader = () => {
+  const { loading, error, data } = useQuery(CATEGORIES);
+
+  if (error) return <p>Error</p>;
+  if (loading) return <p>Loading....</p>;
+
   return (
     <div className="site-header">
       <Link to="/">
         <h1>React Reviews</h1>
       </Link>
+      <nav className="categories">
+        <span>Filter reviews by category:</span>
+        {data.categories.map((category) => (
+          <Link key={category.id} to={`/category/${category.id}`}>
+            {category.name}
+          </Link>
+        ))}
+      </nav>
     </div>
   );
 };
