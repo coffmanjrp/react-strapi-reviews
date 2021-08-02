007d7d3273d177f2ddf5d596426e6f0adf895956
coffmanjrp <coffmanjrp@gmail.com>
2021-08-01 23:24:54 -0700

    Use useQuery hook

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,19 @@
 import { Link } from 'react-router-dom';
-import useFetch from '../hooks/useFetch';
+import { useQuery, gql } from '@apollo/client';
+
+const REVIEWS = gql`
+  query GetReviews {
+    reviews {
+      id
+      title
+      rating
+      body
+    }
+  }
+`;
 
 const HomePage = () => {
-  const { loading, error, data } = useFetch('/reviews');
+  const { loading, error, data } = useQuery(REVIEWS);
 
   if (error) return <p>Error</p>;
   if (loading) return <p>Loading....</p>;
@@ -11,7 +22,7 @@ const HomePage = () => {
 
   return (
     <div>
-      {data.map((review) => (
+      {data.reviews.map((review) => (
         <div key={review.id} className="review-card">
           <div className="rating">{review.rating}</div>
           <h2>{review.title}</h2>
